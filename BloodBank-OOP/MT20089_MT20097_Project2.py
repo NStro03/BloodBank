7b3c106fe1c7cb6f5a111bd1903e157b05572cb1
NishkarshSaxena <nishkarsh20089@iiitd.ac.in>
2020-12-05 22:01:36 +0530

    Added Custome and Staff classes extending User class. Also implemented their getters and setters.

--- BloodBank-OOP/MT20089_MT20097_Project2.py
+++ BloodBank-OOP/MT20089_MT20097_Project2.py
@@ -44,5 +44,70 @@ class User:
         self.__bloodGroup = bgrp
 
 
-# class Customer(User):
+class Customer(User):
+    __customerID = None
+    __dUnits = None
+    __rUnits = None
+
+    def __init__(self, cid, name, age, phno, gender, bgrp):
+        super().__init__(name, age, phno, gender, bgrp)
+        self.__customerID = cid
+        self.__dUnits = 0
+        self.__rUnits = 0
+
+    def getCustomerId(self):
+        return self.__customerID
+
+    def setCustomerId(self, customerid):
+        self.__customerID = customerid
+
+    def getDonatedUnits(self):
+        return self.__dUnits
+
+    def setDonatedUnits(self, dunits):
+        self.__dUnits = dunits
+
+    def getRequestedunits(self):
+        return self.__rUnits
+
+    def setRequestedUnits(self, runits):
+        self.__rUnits = runits
+
+    def getElligibleFreeUnitsCount(self):
+        if self.__dUnits > self.__rUnits:
+            return self.__dUnits - self.__rUnits
+        else:
+            return 0
+
+
+class Staff(User):
+    __staffID = None
+    __acSample = None
+    __rjSample = None
+
+    def __init__(self, staffid, name, age, phno, gender, bgrp):
+        super().__init__(name, age, phno, gender, bgrp)
+        self.__staffID = staffid
+        self.__acSample = 0
+        self.__rjSample = 0
+
+    def getStaffID(self):
+        return self.__staffID
+
+    def setStaffID(self, sid):
+        self.__staffID = sid
+
+    def getAcceptedSampleCount(self):
+        return self.__acSample
+
+    def incrementAcceptedSampleCount(self):
+        self.__acSample = self.__acSample + 1
+
+    def getRejectedSampleCount(self):
+        return self.__rjSample
+
+    def incrementRejectedSampleCount(self):
+        self.__rjSample = self.__rjSample + 1
+
+
 
